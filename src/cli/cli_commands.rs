8100e5e775e0b5ac77e9c4ac1af41a5e5f5352d3
Niklas <26083646+Blinningjr@users.noreply.github.com>
2022-10-01 14:16:07 +0200

    Update dependencies (#9)
    * Update dependencies
    
    * Update clap dependencies

--- src/cli/cli_commands.rs
+++ src/cli/cli_commands.rs
@@ -36,7 +36,7 @@ const HALT_SUB_CMD: &str = "halt";
 const RESET_SUB_CMD: &str = "reset";
 const STEP_SUB_CMD: &str = "step";
 
-fn set_breakpoint_command() -> Command<'static> {
+fn set_breakpoint_command() -> Command {
     Command::new(BKPT_SUB_CMD)
         .about("Set breakpoint")
         .alias("b")
@@ -49,20 +49,20 @@ fn set_breakpoint_command() -> Command<'static> {
     // TODO: Set as pathbuf
 }
 
-fn clear_breakpoint_command() -> Command<'static> {
+fn clear_breakpoint_command() -> Command {
     Command::new(CLEAR_SUB_CMD)
         .about("Clear breakpoint")
         .alias("c")
         .arg_required_else_help(true)
 }
 
-fn clear_breakpoints_command() -> Command<'static> {
+fn clear_breakpoints_command() -> Command {
     Command::new(CLEAR_ALL_SUB_CMD)
         .about("Clear all breakpoints")
         .alias("ca")
 }
 
-fn breakpoint_subcommands() -> [Command<'static>; 3] {
+fn breakpoint_subcommands() -> [Command; 3] {
     [
         set_breakpoint_command(),
         clear_breakpoint_command(),
@@ -70,7 +70,7 @@ fn breakpoint_subcommands() -> [Command<'static>; 3] {
     ]
 }
 
-fn breakpoint_commands() -> Command<'static> {
+fn breakpoint_commands() -> Command {
     Command::new(BREAKPOINT_CMD)
         .about("Collection of breakpoint commands")
         .alias("b")
@@ -78,7 +78,7 @@ fn breakpoint_commands() -> Command<'static> {
         .subcommands(breakpoint_subcommands())
 }
 
-fn attach_command() -> Command<'static> {
+fn attach_command() -> Command {
     Command::new(ATTACH_SUB_CMD)
         .about("Attach debugger to target")
         .alias("a")
@@ -98,13 +98,13 @@ fn attach_command() -> Command<'static> {
         )
 }
 
-fn continue_command() -> Command<'static> {
+fn continue_command() -> Command {
     Command::new(CONTINUE_SUB_CMD)
         .about("Continue halted program")
         .alias("c")
 }
 
-fn flash_command() -> Command<'static> {
+fn flash_command() -> Command {
     Command::new(FLASH_SUB_CMD)
         .about("Flash target")
         .alias("f")
@@ -117,13 +117,13 @@ fn flash_command() -> Command<'static> {
         )
 }
 
-fn halt_command() -> Command<'static> {
+fn halt_command() -> Command {
     Command::new(HALT_SUB_CMD)
         .about("Halt running program")
         .alias("h")
 }
 
-fn reset_command() -> Command<'static> {
+fn reset_command() -> Command {
     Command::new(RESET_SUB_CMD)
         .about("Reset target")
         .alias("r")
@@ -136,13 +136,13 @@ fn reset_command() -> Command<'static> {
         )
 }
 
-fn step_command() -> Command<'static> {
+fn step_command() -> Command {
     Command::new(STEP_SUB_CMD)
         .about("Step one machine code instruction")
         .alias("s")
 }
 
-fn target_subcommands() -> [Command<'static>; 6] {
+fn target_subcommands() -> [Command; 6] {
     [
         attach_command(),
         continue_command(),
@@ -153,7 +153,7 @@ fn target_subcommands() -> [Command<'static>; 6] {
     ]
 }
 
-fn target_commands() -> Command<'static> {
+fn target_commands() -> Command {
     Command::new(TARGET_CMD)
         .about("Collection of target commands")
         .alias("t")
@@ -161,7 +161,7 @@ fn target_commands() -> Command<'static> {
         .subcommands(target_subcommands())
 }
 
-fn binary_command() -> Command<'static> {
+fn binary_command() -> Command {
     Command::new(BINARY_SUB_CMD)
         .about("Set file path to binary")
         .alias("b")
@@ -172,7 +172,7 @@ fn binary_command() -> Command<'static> {
         ) // TODO: Set as pathbuf
 }
 
-fn chip_command() -> Command<'static> {
+fn chip_command() -> Command {
     Command::new(CHIP_SUB_CMD)
         .about("Set chip model")
         .alias("c")
@@ -183,7 +183,7 @@ fn chip_command() -> Command<'static> {
         )
 }
 
-fn probe_command() -> Command<'static> {
+fn probe_command() -> Command {
     Command::new(PROBE_SUB_CMD)
         .about("Set probe to debug with")
         .alias("c")
@@ -195,7 +195,7 @@ fn probe_command() -> Command<'static> {
         )
 }
 
-fn work_directory_command() -> Command<'static> {
+fn work_directory_command() -> Command {
     Command::new(WORK_DIR_SUB_CMD)
         .about("Specify source code work directory")
         .alias("wd")
@@ -204,7 +204,7 @@ fn work_directory_command() -> Command<'static> {
     // TODO: Set as pathbuf
 }
 
-fn config_subcommands() -> [Command<'static>; 4] {
+fn config_subcommands() -> [Command; 4] {
     [
         binary_command(),
         chip_command(),
@@ -213,7 +213,7 @@ fn config_subcommands() -> [Command<'static>; 4] {
     ]
 }
 
-fn config_commands() -> Command<'static> {
+fn config_commands() -> Command {
     Command::new(CONFIG_CMD)
         .about("Collection of configuration commands")
         .alias("c")
@@ -221,31 +221,31 @@ fn config_commands() -> Command<'static> {
         .subcommands(config_subcommands())
 }
 
-fn cycles_command() -> Command<'static> {
+fn cycles_command() -> Command {
     Command::new(CYCLES_SUB_CMD)
         .about("Print the value of the cycle counter")
         .alias("c")
 }
 
-fn disassemble_command() -> Command<'static> {
+fn disassemble_command() -> Command {
     Command::new(DISASSEMBLE_SUB_CMD)
         .about("Disassemble code at current location")
         .alias("d")
 }
 
-fn trace_command() -> Command<'static> {
+fn trace_command() -> Command {
     Command::new(TRACE_SUB_CMD)
         .about("Trace cycle counter at breakpoint instructions until `bkpt_end` is reached")
         .alias("t")
 }
 
-fn registers_command() -> Command<'static> {
+fn registers_command() -> Command {
     Command::new(REGISTERS_SUB_CMD)
         .about("Print registers")
         .alias("r")
 }
 
-fn read_command() -> Command<'static> {
+fn read_command() -> Command {
     Command::new(READ_SUB_CMD)
         .about("Read bytes from a memory address")
         .alias("r")
@@ -261,21 +261,21 @@ fn read_command() -> Command<'static> {
         )
 }
 
-fn stack_command() -> Command<'static> {
+fn stack_command() -> Command {
     Command::new(STACK_SUB_CMD).about("Print stack").alias("s")
 }
 
-fn stack_trace_command() -> Command<'static> {
+fn stack_trace_command() -> Command {
     Command::new(STACK_TRACE_SUB_CMD)
         .about("Print stack trace")
         .alias("st")
 }
 
-fn status_command() -> Command<'static> {
+fn status_command() -> Command {
     Command::new(STATUS_SUB_CMD).about("Print target status")
 }
 
-fn variable_command() -> Command<'static> {
+fn variable_command() -> Command {
     Command::new(VARIABLE_SUB_CMD)
         .about("Show a variable")
         .alias("var")
@@ -286,13 +286,13 @@ fn variable_command() -> Command<'static> {
         )
 }
 
-fn variables_command() -> Command<'static> {
+fn variables_command() -> Command {
     Command::new(VARIABLES_SUB_CMD)
         .about("Print all variables in scope")
         .alias("vars")
 }
 
-fn info_subcommands() -> [Command<'static>; 10] {
+fn info_subcommands() -> [Command; 10] {
     [
         cycles_command(),
         disassemble_command(),
@@ -307,7 +307,7 @@ fn info_subcommands() -> [Command<'static>; 10] {
     ]
 }
 
-fn info_commands() -> Command<'static> {
+fn info_commands() -> Command {
     Command::new(INFO_CMD)
         .about("Collection of debug info commands")
         .alias("i")
@@ -315,11 +315,11 @@ fn info_commands() -> Command<'static> {
         .subcommands(info_subcommands())
 }
 
-fn exit_command() -> Command<'static> {
+fn exit_command() -> Command {
     Command::new(EXIT_CMD).about("Exit the debugger").alias("e")
 }
 
-fn all_erdb_commands() -> [Command<'static>; 5] {
+fn all_erdb_commands() -> [Command; 5] {
     [
         breakpoint_commands(),
         config_commands(),
@@ -329,7 +329,7 @@ fn all_erdb_commands() -> [Command<'static>; 5] {
     ]
 }
 
-fn erdb_command() -> Command<'static> {
+fn erdb_command() -> Command {
     command!("ERDB")
         .author("Blinningjr")
         .about("Embedded Rust Debugger")
